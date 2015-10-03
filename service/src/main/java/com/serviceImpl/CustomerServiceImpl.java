86bd61d7f51bbe79878214badb654ea4a45c318b
yoga-chand <yoga.chand@gmail.com>
2015-10-04 01:23:24 +0530

    Update CustomerServiceImpl.java

--- service/src/main/java/com/serviceImpl/CustomerServiceImpl.java
+++ service/src/main/java/com/serviceImpl/CustomerServiceImpl.java
@@ -104,8 +104,10 @@ public class CustomerServiceImpl implements ICustomerService{
 			MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3"); 
         	mongo = new MongoClient(uri);
         	DB db = mongo.getDB(uri.getDatabase());
-        	boolean auth = db.authenticate("yoga", "test123".toCharArray());
-        	System.out.println("db authenticated "+auth);
+        	if(!DataUtils.auth){
+        		DataUtils.auth = db.authenticate("yoga", "test123".toCharArray()); 
+        		System.out.println("db authenticated "+DataUtils.auth);
+        	}
 			DBCollection col = db.getCollection(database);
 			DBObject query = BasicDBObjectBuilder.start().add("mdn", mdn).get();
 			DBCursor cursor = col.find(query);
