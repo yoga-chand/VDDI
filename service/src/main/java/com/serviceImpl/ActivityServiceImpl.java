075c2640d93a630e763446df879059999564347e
yoga-chand <yoga.chand@gmail.com>
2015-10-04 01:23:53 +0530

    Update ActivityServiceImpl.java

--- service/src/main/java/com/serviceImpl/ActivityServiceImpl.java
+++ service/src/main/java/com/serviceImpl/ActivityServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.datasource.DataUtils;
 import com.model.ActivityObj;
 
 import org.codehaus.jackson.map.ObjectMapper;
@@ -66,8 +67,10 @@ public class ActivityServiceImpl {
 			MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3"); 
         	mongo = new MongoClient(uri);
         	DB db = mongo.getDB(uri.getDatabase());
-        	boolean auth = db.authenticate("yoga", "test123".toCharArray());
-        	System.out.println("db authenticated "+auth);
+        	if(!DataUtils.auth){
+        		DataUtils.auth = db.authenticate("yoga", "test123".toCharArray()); 
+        		System.out.println("db authenticated "+DataUtils.auth);
+        	}
 			DBCollection col = db.getCollection("customerdetails");
 			DBObject query = BasicDBObjectBuilder.start().add("mdn", mdn).get();
 			//read example
