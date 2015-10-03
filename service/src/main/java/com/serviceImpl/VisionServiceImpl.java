5e807a5676623e1bbf751bd19a6c0db323392c59
yoga-chand <yoga.chand@gmail.com>
2015-10-04 01:11:34 +0530

    Update VisionServiceImpl.java

--- service/src/main/java/com/serviceImpl/VisionServiceImpl.java
+++ service/src/main/java/com/serviceImpl/VisionServiceImpl.java
@@ -28,7 +28,9 @@ public class VisionServiceImpl implements IVisionService {
 			MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3"); 
         	mongo = new MongoClient(uri);
         	DB db = mongo.getDB(uri.getDatabase());
+			boolean auth = db.authenticate("yoga", "test123".toCharArray());
 			DBCollection col = db.getCollection("usage");
+        	System.out.println("db authenticated "+auth);
 			DBObject query = BasicDBObjectBuilder.start().add("mdn", mdn).get();
 			DBCursor cursor = col.find(query);
 			ObjectMapper objectMapper = new ObjectMapper();
