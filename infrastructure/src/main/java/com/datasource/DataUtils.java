b3e823d29d44a639e7928c615634ff430695bcab
yoga-chand <yoga.chand@gmail.com>
2015-10-04 01:55:24 +0530

    Update DataUtils.java

--- infrastructure/src/main/java/com/datasource/DataUtils.java
+++ infrastructure/src/main/java/com/datasource/DataUtils.java
@@ -1,48 +1,44 @@
 package com.datasource;
 
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
 import com.mongodb.*;
 
 public class DataUtils {
 
-	private static DataUtils dataUtils;
+	
+	public static Map<String, Object> dbMap = new HashMap<String,Object>(); 
 	
 	public static boolean auth = false;
 	
-	private DataUtils(){
-		
-	}
 	
-	public static DataUtils getInstance(){
-		
-		if(dataUtils == null){
-			synchronized (DataUtils.class) {
-				dataUtils = new DataUtils();
-			}
-		}
-		return dataUtils;
-	}
 	
-/*	public static MongoClient getConnection(String database){
-		MongoClient mongo = null;
-		try {
-			
-		   MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3"); 
-        	   mongo = new MongoClient(uri);
-        	   DB db = mongo.getDB(uri.getDatabase());
-		   
-			
+	public static Map<String, Object> getConnection(){
+		
+        MongoClient mongo = null;
+        System.out.println("dbmap size "+dbMap.size());
+        if(dbMap.size()==0){
+		try { 
+			MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3"); 
+			mongo = new MongoClient(uri);
+			DB db = mongo.getDB(uri.getDatabase());
+		    System.out.println("auth status "+DataUtils.auth);
+		    if(!DataUtils.auth){
+		      	DataUtils.auth = db.authenticate("yoga", "test123".toCharArray()); 
+		      	System.out.println("db authenticated "+DataUtils.auth);
+		    }
+		    dbMap.put("mongo", mongo);
+		    dbMap.put("db", db);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return mongo;
+        }
+		return dbMap;
+      
 	}
-		*/
-		
 	
 }
 
