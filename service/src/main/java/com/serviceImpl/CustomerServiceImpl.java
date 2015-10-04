0f72fbdd6d2fe1be7fd246d4274bacb2035207d0
yoga-chand <yoga.chand@gmail.com>
2015-10-04 14:26:01 +0530

    Update CustomerServiceImpl.java

--- service/src/main/java/com/serviceImpl/CustomerServiceImpl.java
+++ service/src/main/java/com/serviceImpl/CustomerServiceImpl.java
@@ -118,7 +118,7 @@ public class CustomerServiceImpl implements ICustomerService{
 			/*MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3"); 
         	mongo = new MongoClient(uri);
         	DB db = mongo.getDB(uri.getDatabase());*/
-        	System.out.println("auth status "+DataUtils.auth);
+        	System.out.println("auth status in custservice impl "+DataUtils.auth);
         	/*if(!DataUtils.auth){
         		DataUtils.auth = db.authenticate("yoga", "test123".toCharArray()); 
         		System.out.println("db authenticated "+DataUtils.auth);
@@ -144,7 +144,7 @@ public class CustomerServiceImpl implements ICustomerService{
 			e.printStackTrace();
 		}
 		finally{
-		//	mongo.close();
+			mongo.close();
 		}
 
 		return customerDetails;
