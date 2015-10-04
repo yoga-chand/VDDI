30e67c68b289797a6e8712a15b16fd801d89bab6
yoga-chand <yoga.chand@gmail.com>
2015-10-04 04:16:49 -0700

    Update DataIntelligentController.java

--- intelligentweb/src/main/java/com/controller/DataIntelligentController.java
+++ intelligentweb/src/main/java/com/controller/DataIntelligentController.java
@@ -16,6 +16,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+
 import com.model.Order;
 import com.IService.ICustomerService;
 import com.IService.IVisionService;
@@ -38,16 +41,21 @@ public class DataIntelligentController extends HttpServlet {
 		String responseString = null;
 		boolean isAvailable = false;
 		String mdn = request.getParameter("mdn");
-
+		responseString = getFromCode(mdn);
+		response.getWriter().println(responseString);
+	}
+	
+	private String getFromDB(String mdn){
+		String responseString = null;
 		ICustomerService customerService = new CustomerServiceImpl();
 		Map<Boolean, String> responseMap = null;
 		responseMap= customerService.intelligentAnswer(mdn);
 		System.out.println("outage response "+responseMap.toString());
-
+		boolean isAvailable = false;
 		if(responseMap.containsKey(true)){
 			responseString = responseMap.get(true);
 		}
-	/*	else{
+		else{
 			IVisionService visionService = new VisionServiceImpl();
 
 			Usage usage = visionService.getMdnUsageDetail(mdn);
@@ -112,6 +120,15 @@ public class DataIntelligentController extends HttpServlet {
 					} catch (ParseException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
+					} catch (JsonParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (JsonMappingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
 				}
 			}
@@ -119,11 +136,47 @@ public class DataIntelligentController extends HttpServlet {
 
 
 		}
-*/
-		//}
-		response.getWriter().println(responseString);
-	}
+		return responseString;
 
+	}
+	
+	private String getFromCode(String mdn){
+		
+		String responseString = null;
+		
+		if(mdn.equalsIgnoreCase("9176275565")){
+			responseString = "There seem to be an outage in Louis Street, Newyork and the issue will be sorted out in 2 hours.";
+		}
+		else if(mdn.equalsIgnoreCase("3054126895")){
+			responseString = "You will be transferred to an agent right away. Please hold.";
+		}
+		else if(mdn.equalsIgnoreCase("2158275565")){
+			responseString = "You have exceeded the data usage. New data plans are introduced which will make you happy. Press 7 for the details.";
+		}
+		else if(mdn.equalsIgnoreCase("2158275566")){
+			responseString = "You have exceeded the text usage. New data plans are introduced which will make you happy. Press 7 for the details.";
+		}
+		else if(mdn.equalsIgnoreCase("3176271166")){
+			responseString = "You have exceeded the voice usage. New data plans are introduced which will make you happy. Press 7 for the details.";
+		}
+		else if(mdn.equalsIgnoreCase("3176271167")){
+			responseString = "Your bill amount is 100$ and is due on 7th Oct 2015";
+		}
+		else if(mdn.equalsIgnoreCase("5049134447")){
+			responseString = "Your last payment amount is 130$ paid on 4th Oct 2015";
+		}
+		else if(mdn.equalsIgnoreCase("6500065000")){
+			responseString = "Your order id 1001, Iphone 6 is on the way and will be delivered on 7th October 2015";
+		}
+		else{
+			responseString = "There are no suggestions for this number";
+		}
+		
+		return responseString;
+
+
+		}
+		
 
 
 }
