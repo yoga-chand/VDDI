45d9b03f721f230991e9b4f195cd55528e58f670
yoga-chand <yoga.chand@gmail.com>
2015-10-04 13:38:54 +0530

    Update DataUtils.java

--- infrastructure/src/main/java/com/datasource/DataUtils.java
+++ infrastructure/src/main/java/com/datasource/DataUtils.java
@@ -23,15 +23,15 @@ public class DataUtils {
         	
         		try { 
         			//MongoClientURI uri  = new MongoClientURI("mongodb://CloudFoundry_omfu0lp3_t4cigvf3_vc5m5ajq:D0pMgRG0Vq4g-thG5E2ERlTzmP_NvlwH@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3");
-        			MongoClientURI uri  = new MongoClientURI("mongodb://yoga:test123@ds051863.mongolab.com:51863/CloudFoundry_omfu0lp3_t4cigvf3");
+        			MongoClientURI uri  = new MongoClientURI("mongodb://yoga:test123@ds051863.mongolab.com:51863/?authSource=CloudFoundry_omfu0lp3_t4cigvf3");
         			mongo = new MongoClient(uri);
         			DB db = mongo.getDB(uri.getDatabase());
-        		    System.out.println("auth status "+DataUtils.auth);
+        		    /*System.out.println("auth status "+DataUtils.auth);
         		    if(!DataUtils.auth){
         		    	DataUtils.auth = true;
         		      	//DataUtils.auth = db.authenticate("yoga", "test123".toCharArray()); 
         		      	System.out.println("db authenticated "+DataUtils.auth);
-        		    }
+        		    }*/
         		    dbMap.put("mongo", mongo);
         		    dbMap.put("db", db);
         		} catch (UnknownHostException e) {
